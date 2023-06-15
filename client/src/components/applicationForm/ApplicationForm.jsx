f3f716720a1a69fb5942f8622fa59ff1300255cd
leonard-porteria <porterialeonard@gmail.com>
2023-06-15 12:45:18 +0800

    appending on specific child object

--- client/src/components/applicationForm/ApplicationForm.jsx
+++ client/src/components/applicationForm/ApplicationForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 import OrganizationInformation from './subForms/organizationInfo/organizationInformation';
 import PersonalInformation from './subForms/personalInfo/personalInformation';
@@ -11,36 +11,106 @@ import ContactInformation from './subForms/contactInfo/contactInformation';
 import './ApplicationForm.scss';
 
 const ApplicationForm = () => {
-  const [response, setResponse] = useState(null);
+  const [applicantData, setApplicantData] = useState({
+    application_status: {
+      applicant_code: '',
+      member_id: '',
+      club_id: '',
+      education_id: '',
+      dependent_id: '',
+      contact_id: '',
+      applcation_status: 'Pending',
+      date_of_application: '',
+    },
+    member_information: {
+      member_id: '',
+      member_name: '',
+      place_of_birth: '',
+      date_of_birth: '',
+      address: '',
+      age: '',
+      sex: '',
+      heigh_in_cm: '',
+      weight_in_kg: '',
+      bmi: '',
+      civil_status: '',
+      nationality: '',
+      religion: '',
+      blood_type: '',
+      telephone_number: '',
+      cellphone_number: '',
+      email: '',
+      sponsor_name: '',
+      sponsor_membership_id: '',
+      work_title_or_position: '',
+      company_code: '',
+    },
+    company: {
+      company_code: '',
+      company_name: '',
+      company_telephone: '',
+      company_email: '',
+      company_address: '',
+    },
+    organization_club: {
+      club_id: '',
+      club_region: '',
+      club_name: '',
+      club_president: '',
+      club_president_member_id: '',
+    },
+    // MULTIVALUES
+    education: [
+      {
+        education_id: '',
+        education_level: '',
+        school_name: '',
+        date_graduated: '',
+        course_strand: '',
+      },
+    ],
+    legal_dependents: [
+      {
+        dependent_id: '',
+        dependent_name: '',
+        dependent_date_of_birth: '',
+        dependent_contact_number: '',
+        dependent_relationship: '',
+      },
+    ],
+    contact_person: [
+      {
+        contact_id: '',
+        contact_name: '',
+        contact_cellphone_number: '',
+        contact_address: '',
+        contact_relationship: '',
+      },
+    ],
+  });
+
+  const handleOnInputChange = useCallback((value) => {
+    console.log(value);
+    setApplicantData((prevData) => ({
+      ...prevData,
+      application_status: {
+        ...prevData.application_status,
+        date_of_application: value.date_of_application,
+      },
+
+      organization_club: {
+        ...prevData.organization_club,
+        club_region: value.club_region,
+        club_name: value.club_name,
+        club_president: value.club_president,
+      },
+    }));
+  });
 
   const handleSubmit = () => {
     console.log('submit');
-    const url = 'http://localhost:5173/api/company';
-    const data = {
-      company_code: 'C01-NR-05',
-      company_name: 'newCompany',
-      company_telephone: '87000',
-      company_email: 'mycompany@email.com',
-      company_address: 'dyan lang st.',
-    };
 
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        // Add any other headers if required
-      },
-      body: JSON.stringify(data), // Convert data to JSON string
-    })
-      .then((response) => response.json()) // Parse response data as JSON
-      .then((result) => {
-        // Handle the result
-        setResponse(result);
-      })
-      .catch((error) => {
-        // Handle errors
-        console.error('Error:', error);
-      });
+    console.log(applicantData);
   };
 
   return (
@@ -53,7 +123,7 @@ const ApplicationForm = () => {
       </div>
 
       <div className='Form'>
-        <OrganizationInformation />
+        <OrganizationInformation onInputChange={handleOnInputChange} />
         <PersonalInformation />
         <EmployeeInformation />
         <EducaitonInformation />
@@ -64,7 +134,6 @@ const ApplicationForm = () => {
         <div className='Form__button' onClick={handleSubmit}>
           <h1 className='Form__button__submit'>SUBMIT APPLICAITON</h1>
         </div>
-        {response && <div>{JSON.stringify(response)}</div>}
       </div>
     </>
   );
--- client/src/components/applicationForm/subForms/organizationInfo/organizationInformation.jsx
+++ client/src/components/applicationForm/subForms/organizationInfo/organizationInformation.jsx
@@ -1,6 +1,47 @@
+import { useState, useEffect } from 'react';
+
 import './organizationInformation.scss';
 
-const OrganizationInformation = () => {
+const OrganizationInformation = ({ onInputChange }) => {
+  const [clubData, setClubData] = useState({
+    club_region: '',
+    club_name: '',
+    club_president: '',
+    date_of_application: '',
+  });
+
+  const handleClubRegion = (event) => {
+    setClubData((prevClub) => ({
+      ...prevClub,
+      club_region: event.target.value,
+    }));
+  };
+
+  const handleClubName = (event) => {
+    setClubData((prevClub) => ({
+      ...prevClub,
+      club_name: event.target.value,
+    }));
+  };
+
+  const handleClubPresident = (event) => {
+    setClubData((prevClub) => ({
+      ...prevClub,
+      club_president: event.target.value,
+    }));
+  };
+
+  const handleDateOfApplication = (event) => {
+    setClubData((prevClub) => ({
+      ...prevClub,
+      date_of_application: event.target.value,
+    }));
+  };
+
+  useEffect(() => {
+    onInputChange(clubData);
+  }, [clubData]);
+
   return (
     <div className='Form__Organization'>
       {/* ORGANIZATION DETAILS */}
@@ -9,19 +50,19 @@ const OrganizationInformation = () => {
       <div className='Form__Organization__container'>
         <label>
           <p>Organization Region:</p>
-          <input type='text' />
+          <input type='text' onBlur={handleClubRegion} />
         </label>
         <label>
           <p>Club Name:</p>
-          <input type='text' />
+          <input type='text' onBlur={handleClubName} />
         </label>
         <label>
           <p>Club President:</p>
-          <input type='text' />
+          <input type='text' onBlur={handleClubPresident} />
         </label>
         <label>
           <p>Date of Application:</p>
-          <input type='text' />
+          <input type='date' onBlur={handleDateOfApplication} />
         </label>
       </div>
 
